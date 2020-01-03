86ee71342c1b65ace2ea40b3936acf13b7a0d0c8
DamianComarch <damian.trzcinski@comarch.pl>
2020-01-03 19:26:11 +0100

    local env variables configured

--- ethereum/backendTxSign.js
+++ ethereum/backendTxSign.js
@@ -4,23 +4,16 @@ import web3 from "./web3";
 
 export default async value => {
   const addMessageAbi = newsInbox.methods.addMessage(value).encodeABI();
-  let nonce = await web3.eth.getTransactionCount(
-    "0x7382cda4162b587046caaadb751f6a16e5dd4d84"
-  );
+  let nonce = await web3.eth.getTransactionCount(process.env.ETH_ADDRESS);
   let txParams = {
     nonce: nonce,
     gasPrice: 80 * 100000000,
     gas: 1000000,
-    to: "0xa4c3FE660E474a85F56eD636DE71D55393d48064",
+    to: process.env.CONTRACT_ADDRESS,
     data: addMessageAbi
   };
   const transaction = new Transaction(txParams, { chain: "rinkeby" });
-  transaction.sign(
-    Buffer.from(
-      "B864B1BF5A5310F113752EBF34EFC275C29A6EA2480173ADE4C3BE137B6DF53F",
-      "hex"
-    )
-  );
+  transaction.sign(Buffer.from(process.env.PRIVATE_KEY, "hex"));
   const serializedTransaction = transaction.serialize();
   const receipt = await web3.eth.sendSignedTransaction(
     "0x" + serializedTransaction.toString("hex")
--- ethereum/newsInbox.js
+++ ethereum/newsInbox.js
@@ -1,9 +1,11 @@
 import web3 from "./web3";
 import newsInbox from "./build/newsInbox.json";
 
+console.log(process.env);
+console.log(process.env.CONTRACT_ADDRESS);
 const instance = new web3.eth.Contract(
   newsInbox.abi,
-  "0xa4c3FE660E474a85F56eD636DE71D55393d48064"
+  process.env.CONTRACT_ADDRESS
 );
 
 export default instance;
